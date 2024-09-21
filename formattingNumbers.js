033487c0aa77b6a636b5a1e8577837abb7c28cf5
Anmol Behl <37472462+behl1anmol@users.noreply.github.com>
2024-09-21 04:55:52 +0000

    Adding Type Conversion Code Files

--- formattingNumbers.js
+++ formattingNumbers.js
@@ -6,4 +6,46 @@ console.log(`Round: ${Math.floor(num1)}`);
 
 //fixing number of decimal places
 let fixed = num1.toFixed(3);
-console.log(`Type of Fixed: ${typeof(fixed)} Fixed Number: ${fixed}`);
+console.log(`Type of Fixed: ${typeof fixed} Fixed Number: ${fixed}`);
+
+//Display in Locale Specific format
+let num2 = 1_000_000;
+console.log(`USA: ${num2.toLocaleString("en-US")}`);
+console.log(`USA: ${num2.toLocaleString("el-EL")}`);
+console.log(`USA: ${num2.toLocaleString("bg-BG")}`);
+console.log(`USA: ${num2.toLocaleString("pa-PA")}`);
+
+//Intl Format Currency
+let salary = 100000;
+let monthlySalary = salary / 12;
+console.log(`Monthly Salary: ${monthlySalary.toFixed(2)}`);
+
+let formatter1 = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+console.log(`US Dollars: ${formatter1.format(monthlySalary)}`);
+
+let formatter2 = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "EUR",
+});
+console.log(`Euros : ${formatter2.format(monthlySalary)}`);
+
+let formatter3 = new Intl.NumberFormat("ja-JA", {
+  style: "currency",
+  currency: "JPY",
+});
+console.log(`Yen: ${formatter3.format(monthlySalary)}`);
+
+let formatter4 = new Intl.NumberFormat("zk-HK", {
+  style: "currency",
+  currency: "HKD",
+});
+console.log(`Hong Kong Dollars: ${formatter4.format(monthlySalary)}`);
+
+let formatter5 = new Intl.NumberFormat("en-IN", {
+  style: "currency",
+  currency: "INR",
+});
+console.log(`Rupee: ${formatter5.format(monthlySalary)}`);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const module1 = require("./formattingNumbers.js");
+const module1 = require("./formattingDates.js");
 
 // Example usage
 //module1.exampleA();
